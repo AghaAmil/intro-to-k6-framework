ecd7be20997717e986630fcd1292a486a6240619
AghaAmil <moravveji.amir.92@gmail.com>
2024-07-19 21:31:51 +0330

    add comments to first script

--- script-files/first-script.js
+++ script-files/first-script.js
@@ -1,12 +1,14 @@
 import http from "k6/http";
 import { sleep } from "k6";
 
+// setting a configuration for load test
 export const options = {
-  vus: 10,
-  duration: "10s",
+  vus: 10, // number of virtual users
+  duration: "10s", // load test duration
 };
 
 export default function () {
   http.get("https://test.k6.io");
-  sleep(1);
+  // pausing after a request is sent
+  sleep(1); // pauses for 1 second
 }
