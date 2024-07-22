613a9c9336d4aea3ca02318dc311730c14a7aec6
AghaAmil <moravveji.amir.92@gmail.com>
2024-07-22 18:21:24 +0330

    correct and enhance the smoke test

--- script-files/smoke-test.js
+++ script-files/smoke-test.js
@@ -1,17 +1,33 @@
 import http from "k6/http";
 import { sleep } from "k6";
 
-// setting a configuration for load test
+// setting a configuration for a smoke test
 export const options = {
   vus: 1, // number of virtual users
   duration: "30s", // load test duration
 };
 
 export default function () {
+  /*
+  check the first page
+  pauses for a second 
+  check the next page
+  pauses for the two seconds
+  ....
+  */
+
   http.get("https://test.k6.io");
   sleep(1); // pauses for 1 second
-  http.get("<https://test.k6.io/contacts.php>");
-  sleep(2);
-  http.get("<https://test.k6.io/news.php>");
+  http.get("https://test.k6.io/contacts.php");
+  sleep(2); // pauses for 2 seconds
+  http.get("https://test.k6.io/news.php");
   sleep(2);
 }
+
+/*
+
+creating failure scenarios:
+1. change the first request to .local and check the result
+2. change the second request to contact.php to cause 404 error
+
+*/
