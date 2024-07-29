b698cce0157626a001a4678f32b120fe55e78502
AghaAmil <moravveji.amir.92@gmail.com>
2024-07-29 22:26:31 +0330

    comment correction

--- script-files/2-creating-test-scripts-with-k6/5-metric-types-and-aggregation-methods.js
+++ script-files/2-creating-test-scripts-with-k6/5-metric-types-and-aggregation-methods.js
@@ -9,7 +9,7 @@ export const options = {
     http_req_duration: ["max < 1000"], // max duration request should be less than 1 second
     http_req_failed: ["rate<0.01"], // rate of failure should be less than 1%
     http_reqs: ["count>20"], // the number of total requests should be grater than 20
-    http_reqs: ["rate>4"], // the number of total requests per second should be grater than 20
+    http_reqs: ["rate>3"], // the number of total requests per second should be grater than 3
     vus: ["value>9"], // the number of virtual users should be grater than 9
   },
 };
