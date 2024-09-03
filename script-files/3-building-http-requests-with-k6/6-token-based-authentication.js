5db9d07eb295fcd779a3359274831146f17ede6e
AghaAmil <moravveji.amir.92@gmail.com>
2024-09-03 15:00:30 +0330

    refactoring the code 6-token-based-authentication

--- script-files/3-building-http-requests-with-k6/6-token-based-authentication.js
+++ script-files/3-building-http-requests-with-k6/6-token-based-authentication.js
@@ -1,34 +1,47 @@
 import http from "k6/http";
 
 export default function () {
-  // creating the body of API request
+  // Create the body of the API request
   const credential = {
-    // the date in Unix Epoch format will be added to username in order to create unique IDs
+    // The date in Unix Epoch format will be added to the username to create unique IDs
     username: `test_${Date.now()}`,
     password: `secret_${Date.now()}`,
   };
 
-  // headers is the same for both requests
+  // Define headers for both requests
   const params = {
     headers: {
       "Content-Type": "application/json",
     },
   };
 
-  const resCreation = http.post("https://test-api.k6.io/user/register/", JSON.stringify(credential), params);
+  // Step 1: User registration
+  const resCreation = http.post(
+    "<https://test-api.k6.io/user/register/>",
+    JSON.stringify(credential),
+    params
+  );
 
-  const resToken = http.post("https://test-api.k6.io/auth/token/login/", JSON.stringify(credential), params);
+  // Step 2: User login to obtain the token
+  const resToken = http.post(
+    "<https://test-api.k6.io/auth/token/login/>",
+    JSON.stringify(credential),
+    params
+  );
 
+  // Extract username and token from the responses
   let userName = resCreation.json().username;
   let Token = resToken.json().access;
   console.log(userName);
   console.log(Token);
 
-  const resPrivate = http.get("https://test-api.k6.io/my/crocodiles/", {
+  // Step 3: Access the private endpoint using the token
+  const resPrivate = http.get("<https://test-api.k6.io/my/crocodiles/>", {
     headers: {
       Authorization: `Bearer ${Token}`,
     },
   });
 
+  // Log the response from the private endpoint
   console.log(resPrivate.body);
 }
