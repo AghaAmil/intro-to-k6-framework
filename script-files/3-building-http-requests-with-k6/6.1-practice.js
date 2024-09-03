05c68f3dc53d3844e4c384637cc41d2846f6b5ac
AghaAmil <moravveji.amir.92@gmail.com>
2024-09-03 16:22:07 +0330

    adding practice codes for token-based auth

---
+++ script-files/3-building-http-requests-with-k6/6.1-practice.js
@@ -0,0 +1,41 @@
+import http from "k6/http";
+
+export default function () {
+  // creating the body of API request
+  const credential = {
+    // the date in Unix Epoch format will be added to username in order to create unique IDs
+    username: `test_${Date.now()}`,
+    password: `secret_${Date.now()}`,
+  };
+
+  // headers is the same for both requests
+  const params = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  const resCreation = http.post("https://test-api.k6.io/user/register/", JSON.stringify(credential), params);
+  const resToken = http.post("https://test-api.k6.io/auth/token/login/", JSON.stringify(credential), params);
+
+  let userName = resCreation.json().username;
+  let accessToken = resToken.json().access;
+
+  const resCrocodileCreation = http.post(
+    "https://test-api.k6.io/my/crocodiles/",
+    JSON.stringify({
+      name: "Crocs Khafan",
+      sex: "M",
+      date_of_birth: "1990-09-15",
+    }),
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }
+  );
+
+  console.log(userName);
+  console.log(resCrocodileCreation.json());
+}
