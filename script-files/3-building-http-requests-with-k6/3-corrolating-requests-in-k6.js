1e836fcc73f41d13eda913e2d976ebd8c60c907b
AghaAmil <moravveji.amir.92@gmail.com>
2024-09-03 11:52:41 +0330

    correlating requests

---
+++ script-files/3-building-http-requests-with-k6/3-corrolating-requests-in-k6.js
@@ -0,0 +1,16 @@
+import http from "k6/http";
+import { check } from "k6";
+
+export default function () {
+  let res = http.get("https://test-api.k6.io/public/crocodiles/");
+  let crocodiles = res.json(); // Convert the list of all crocodiles to a JSON object
+  let crocodileId = crocodiles[0]["id"]; // Get the ID of the first crocodile
+  let crocodileName = crocodiles[0]["name"]; // Get the name of the first crocodile
+
+  res = http.get(`https://test-api.k6.io/public/crocodiles/${crocodileId}/`);
+
+  check(res, {
+    "Status is 200": (r) => r.status === 200,
+    "Crocodile's name ": (r) => r.json().name === crocodileName,
+  });
+}
