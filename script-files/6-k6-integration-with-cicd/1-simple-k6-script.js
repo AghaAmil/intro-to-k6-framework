ce31a57b015a40f9879edf28db0fad524fe41a4a
AghaAmil <moravveji.amir.92@gmail.com>
2024-09-05 16:08:29 +0330

    k6 cli exit code

---
+++ script-files/6-k6-integration-with-cicd/1-simple-k6-script.js
@@ -0,0 +1,13 @@
+import http from "k6/http";
+import { sleep } from "k6";
+
+export const options = {
+  thresholds: {
+    http_req_duration: ["p(95)<30"],
+  },
+};
+
+export default function () {
+  http.get("https://test.k6.io");
+  sleep(1);
+}
