ff054bb42d6234733c340a2a50fc19a5347d28c4
AghaAmil <moravveji.amir.92@gmail.com>
2024-09-17 21:42:09 +0330

    improve code comment and code structure

--- script-files/1-different-test-types-with-k6/0-first-script.js
+++ script-files/1-different-test-types-with-k6/0-first-script.js
@@ -1,13 +1,13 @@
 import http from "k6/http";
-import { sleep } from "k6";
+import {sleep} from "k6";
 
 // Configuration for the load test
 export const options = {
-  vus: 10,           // Number of virtual users
-  duration: "10s",    // Duration of the load test
+    vus: 10,           // Number of virtual users
+    duration: "10s",    // Duration of the load test
 };
 
 export default function () {
-  http.get("https://test.k6.io");  // HTTP GET request to test endpoint
-  sleep(1);  // Pauses for 1 second between requests
+    http.get("https://test.k6.io");  // HTTP GET request to test endpoint
+    sleep(1);  // Pauses for 1 second between requests
 }
--- script-files/1-different-test-types-with-k6/1-smoke-test.js
+++ script-files/1-different-test-types-with-k6/1-smoke-test.js
@@ -1,33 +1,32 @@
 import http from "k6/http";
-import { sleep } from "k6";
+import {sleep} from "k6";
 
 // setting a configuration for a smoke test
 export const options = {
-  vus: 1, // number of virtual users
-  duration: "30s", // load test duration
+    vus: 1, // number of virtual users
+    duration: "30s", // load test duration
 };
 
 export default function () {
-  /*
-  check the first page
-  pauses for a second 
-  check the next page
-  pauses for the two seconds
-  ....
-  */
+    /*
+    check the first page
+    pauses for a second
+    check the next page
+    pauses for the two seconds
+    ....
+    */
 
-  http.get("https://test.k6.io");
-  sleep(1); // pauses for 1 second
-  http.get("https://test.k6.io/contacts.php");
-  sleep(2); // pauses for 2 seconds
-  http.get("https://test.k6.io/news.php");
-  sleep(2);
+    // invalid url cause the test to fail
+    // http.get("https://test.k6.local");
+
+    http.get("https://test.k6.io");
+    sleep(1); // pauses for 1 second
+
+    // below url cannot be found, therefore cause 404 error
+    // http.get("https://test.k6.io/contact.php")
+
+    http.get("https://test.k6.io/contacts.php");
+    sleep(2); // pauses for 2 seconds
+    http.get("https://test.k6.io/news.php");
+    sleep(2);
 }
-
-/*
-
-creating failure scenarios:
-1. change the first request to .local and check the result
-2. change the second request to contact.php to cause 404 error
-
-*/
