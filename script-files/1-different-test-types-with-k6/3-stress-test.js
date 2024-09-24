b252ad4324bb38485477f50b83df7b847736f3ff
AghaAmil <moravveji.amir.92@gmail.com>
2024-09-24 21:09:29 +0330

    code structure change

--- script-files/1-different-test-types-with-k6/3-stress-test.js
+++ script-files/1-different-test-types-with-k6/3-stress-test.js
@@ -1,32 +1,32 @@
 import http from "k6/http";
-import { sleep } from "k6";
+import {sleep} from "k6";
 
 // setting a configuration for a stress test
 export const options = {
-  stages: [
-    /* 
+    stages: [
+        /*
 
-    below can be referred as actual values to put a load test for an application
-    { duration: "5m", target: 1000 },
-    { duration: "30m", target: 1000},
-    { duration: "5m", target: 0 },
+        below can be referred as actual values to put a load test for an application
+        { duration: "5m", target: 1000 },
+        { duration: "30m", target: 1000},
+        { duration: "5m", target: 0 },
 
 
-    */
-    // Ramp-up stage
-    { duration: "10s", target: 1000 }, // Ramp up to 1000 users over 10 seconds
-    // Steady state stage
-    { duration: "30s", target: 1000 }, // Stay at 1000 users for 30 seconds
-    // Ramp-down stage
-    { duration: "10s", target: 0 }, // Ramp down to 0 users over 10 seconds
-  ],
+        */
+        // Ramp-up stage
+        {duration: "10s", target: 20}, // Ramp up to 20 users over 10 seconds
+        // Steady state stage
+        {duration: "30s", target: 20}, // Stay at 20 users for 30 seconds
+        // Ramp-down stage
+        {duration: "10s", target: 0}, // Ramp down to 0 users over 10 seconds
+    ],
 };
 
 export default function () {
-  http.get("https://test.k6.io");
-  sleep(1);
-  http.get("https://test.k6.io/contacts.php");
-  sleep(1);
-  http.get("https://test.k6.io/news.php");
-  sleep(1);
+    http.get("https://test.k6.io");
+    sleep(1);
+    http.get("https://test.k6.io/contacts.php");
+    sleep(1);
+    http.get("https://test.k6.io/news.php");
+    sleep(1);
 }
--- script-files/1-different-test-types-with-k6/6-soak-test.js
+++ script-files/1-different-test-types-with-k6/6-soak-test.js
@@ -1,23 +1,23 @@
 import http from "k6/http";
-import { sleep } from "k6";
+import {sleep} from "k6";
 
 // setting a configuration for a load test
 export const options = {
-  stages: [
-    // Ramp-up stage
-    { duration: "5m", target: 1000 }, // Ramp up to 1000 users over 5 minutes
-    // Steady state stage
-    { duration: "24h", target: 1000 }, // Stay at 1000 users for 24 hours
-    // Ramp-down stage
-    { duration: "5m", target: 0 }, // Ramp down to 0 users over 5 minutes
-  ],
+    stages: [
+        // Ramp-up stage
+        {duration: "5m", target: 1000}, // Ramp up to 1000 users over 5 minutes
+        // Steady state stage
+        {duration: "24h", target: 1000}, // Stay at 1000 users for 24 hours
+        // Ramp-down stage
+        {duration: "5m", target: 0}, // Ramp down to 0 users over 5 minutes
+    ],
 };
 
 export default function () {
-  http.get("https://test.k6.io");
-  sleep(1);
-  http.get("https://test.k6.io/contacts.php");
-  sleep(1);
-  http.get("https://test.k6.io/news.php");
-  sleep(1);
+    http.get("https://test.k6.io");
+    sleep(1);
+    http.get("https://test.k6.io/contacts.php");
+    sleep(1);
+    http.get("https://test.k6.io/news.php");
+    sleep(1);
 }
