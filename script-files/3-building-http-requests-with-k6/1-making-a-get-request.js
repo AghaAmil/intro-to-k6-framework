193581d0902af0fdb19a1116e9c96af068647a05
AghaAmil <moravveji.amir.92@gmail.com>
2024-11-02 19:58:16 +0330

    changing code structure

--- script-files/3-building-http-requests-with-k6/1-making-a-get-request.js
+++ script-files/3-building-http-requests-with-k6/1-making-a-get-request.js
@@ -4,3 +4,5 @@ export default function () {
   const res = http.get("https://test-api.k6.io/public/crocodiles/");
   console.log(res);
 }
+
+// read document (5-debugging-http-requests-and-response-in-k6) and redo the script
